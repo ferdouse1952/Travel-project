ff5b53389435cdefa93fb80049e2bcd8bb37fe96
Ferdouse Binte Asad <ferdousebinteasad@gmail.com>
2023-01-27 02:48:14 +0600

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import Home from "./Components/Home/Home";
+import Popular from "./Components/Popular/Popular";
+import Offer from "./Components/Offers/Offer";
+import About from "./Components/About/About";
+import Blog from "./Components/Blog/Blog";
+import Footer from "./Components/Footer/Footer";
 
 function App() {
   return (
-    <div>
-     
-    </div>
+    <>
+      <Navbar />
+      {/* <Home />
+      <Popular />
+      <Offer />
+      <About />
+      <Blog />
+      <Footer /> */}
+    </>
   );
 }
 
---
+++ src/Components/Popular/Popular.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Popular.css';
+
+const Popular = () => {
+    return (
+        <div>
+            <h1>popular</h1>
+        </div>
+    );
+};
+
+export default Popular;
